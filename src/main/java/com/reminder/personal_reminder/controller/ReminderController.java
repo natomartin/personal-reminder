7934660486048fb8efeb6d1dfb81559242dfdc08
natomartin <renato_martin09@gmail.com>
2025-12-29 21:13:22 -0500

    consumo de api get para obtener todos los reminders y pequeño diseño

--- src/main/java/com/reminder/personal_reminder/controller/ReminderController.java
+++ src/main/java/com/reminder/personal_reminder/controller/ReminderController.java
@@ -3,6 +3,7 @@ package com.reminder.personal_reminder.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 
 
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 @RequestMapping("api/reminder")
 public class ReminderController {
